02c2af23d5ec70dd0be14c5a493207d70f526b33
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2020-07-28 00:53:21 -0300

    update rapido

---
+++ access-api.php
@@ -0,0 +1,15 @@
+<?php
+    $url_request = "https://api.hgbrasil.com/finance?format=json&key=00bb4207";
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'rohit');
+    curl_setopt($ch, CURLOPT_URL, $url_request);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    $resultArray = json_decode($result);
+    header('Content-Type: application/json');
+    echo $result;
+?>
